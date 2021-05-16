6a37f2443641b6d738517ff7fec49b5a87a5f58a
Daniel Díaz <diaz_carrete@yahoo.com>
2021-05-16 18:05:29 +0200

    changed name of class

--- lib/Moo/Prelude.hs
+++ lib/Moo/Prelude.hs
@@ -15,18 +15,18 @@ import Moo
 import Data.Kind
 import GHC.TypeLits
 
-type Multicurryable :: Type -> Constraint
-class Multicurryable curried where
+type Call :: Type -> Constraint
+class Call curried where
     type LiftedD curried :: Type
     call :: (E -> curried) -> LiftedD curried
 
-instance Multicurryable (D r) where
+instance Call (D r) where
     type LiftedD (D r) = M r
     call extractor = do
         e <- askE
         liftD $ extractor e
 
-instance Multicurryable curried' => Multicurryable (a -> curried') where
+instance Call curried' => Call (a -> curried') where
     type LiftedD (a -> curried') = a -> LiftedD curried'
     call extractor a = 
         let extractor' = \e -> extractor e a

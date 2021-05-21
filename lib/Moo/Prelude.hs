8f671b1de377dede0f22b1eb0104c815c5ef1686
Daniel Díaz <diaz_carrete@yahoo.com>
2021-05-21 17:25:06 +0200

    links

--- lib/Moo/Prelude.hs
+++ lib/Moo/Prelude.hs
@@ -24,7 +24,7 @@ type Call :: Type -> Constraint
 class Call curried where
     type LiftedD curried :: Type
     -- | Given a way of extracting from the environment 'E' a @curried@
-    -- function that ends in a 'D' action, lift the D-function into the main
+    -- function that ends in a 'D' action, lift the 'D'-function into the main
     -- monad 'M'.
     self :: (E -> curried) -> LiftedD curried
 
@@ -40,9 +40,9 @@ instance Call curried' => Call (a -> curried') where
         let extractor' = \e -> extractor e a
         in self @curried' extractor'
 
--- | Given an environment that 'Has' a @component@, and a way of extracting
+-- | Given an environment that 'Control.Monad.Dep.Has' a @component@, and a way of extracting
 -- from the component a @curried@ function that ends in a 'D' action, lift the
--- function into the main monad 'M'.
+-- 'D'-function into the main monad 'M'.
 call :: forall component curried . (Has component D E, Call curried) => (component D -> curried) -> LiftedD curried
 call extractor = self (extractor . dep @component)
 

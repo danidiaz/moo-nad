2cb7797e03f1eb8e48c251489fd2dffeccaef469
Daniel Díaz <diaz_carrete@yahoo.com>
2021-05-22 10:10:48 +0200

    comments

--- lib-example-impl/Moo.hs
+++ lib-example-impl/Moo.hs
@@ -13,19 +13,24 @@ import Control.Monad.Dep.Has
 
 type M = ReaderT EnvIO IO
 type E = EnvIO
+type D = IO
+liftD :: D x -> M x
+liftD = lift
 
+-- | Same definition as the one in the module signature.
 class HasLogger d e | e -> d where
     logger :: e -> Int -> String -> d ()
 
+-- | Same definition as the one in the module signature.
 data Counter d = Counter { 
         askCounter :: d Int,
         incCounter :: Int -> d ()
     }
 
-type D = IO
-liftD :: D x -> M x
-liftD = lift
-
+-- | This is the point at which we define a concrete type for the environment.
+--
+-- The 'HasLogger' instance is backed by a bare function defined at the top
+-- level, while the 'Counter' component is a field by itself.
 data EnvIO = EnvIO {
         _logger :: Int -> String -> IO (),
         _counter :: Counter IO
@@ -34,6 +39,9 @@ data EnvIO = EnvIO {
 instance HasLogger D E where
     logger (EnvIO {_logger}) = _logger
 
+-- | 'Control.Monad.Dep.Has' establishes a relationship between the environment 'E',
+-- the nominal record defining a component, and the effect monad the component
+-- uses in the environment..
 instance Has Counter D E where
     dep (EnvIO {_counter}) = _counter
 
